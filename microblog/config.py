8a0a7513ed5f8bcfeb1dd1865a6d9ea8b2b11d16
Jesse.Ssempijja <jesse.ssempijja@postbank.co.ug>
2020-06-26 13:34:43 +0300

    added a web form

---
+++ microblog/config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    #secret key configuration
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-cant-touch-this'
+
+    #Database connection
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'
+    + os.path.join(basedir, 'app.db')
+
+    #Database chaneg alerts
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
