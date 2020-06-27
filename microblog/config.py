326d531615293bd6bfaa684974a1d8ed2064982f
Jesse.Ssempijja <jesse.ssempijja@postbank.co.ug>
2020-06-27 12:58:56 +0300

    Added database models

--- microblog/config.py
+++ microblog/config.py
@@ -6,8 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-cant-touch-this'
 
     #Database connection
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'
-    + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+ os.path.join(basedir, 'app.db')
 
     #Database chaneg alerts
     SQLALCHEMY_TRACK_MODIFICATIONS = False
