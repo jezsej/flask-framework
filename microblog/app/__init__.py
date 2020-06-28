4e380843b26b648c9c40b13dcebe57e86b37f1a2
Jesse Ssempijja <jessessempijja@gmail.com>
2020-06-28 17:30:13 +0300

    added register module, login functionality

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app =  Flask(__name__)
+#add configuration file to app
 app.config.from_object(Config)
+
+#instantiate the application db
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+#Login logic for the app
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,12 +1,16 @@
-from flask import render_template, redirect,flash, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, redirect,flash, url_for,  request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user,login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username':'Jensen'}
+    
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,14 +21,50 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('login requested for user {}, remember me {}'.format(form.username.data, 
-        form.remember_me.data))
-
+    #check if user is authenticated already, redirect to index page if they are
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+
+    #check if the all required form fields are provided, proceed to validate user details if they are
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+
+        #set currently logged in user details for flask_login extention to handle    
+        login_user(user, remember=form.remember_me.data)
+
+        #check if there was a page the user tried to access prior to logging in
+        next_page = request.args.get('next')
+
+        #ensure the next page is on the same site, else give them index page
+        if not next_page or url_parse(next_page).netloc != '':
+            return redirect(url_for('index'))
+
+        return redirect(next_page)
     return render_template('login.html', form=form, title='Sign In')    
+
+@app.route('/logout')
+def logout():
+     logout_user()
+     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)     
